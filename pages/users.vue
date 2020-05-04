a2e3becfcac199c049ec0b44192ca7a62a4b94d3
Genilton Souza <genilton.souza26@gmail.com>
2020-05-04 10:34:32 -0400

    lesson 17

---
+++ pages/users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>The Users view</h1>
+    <input type="text" v-model="userId" />
+    <button @click="onLoadUser">Load user</button>
+    <nuxt-child />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userId: ""
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.userId}`);
+    }
+  }
+};
+</script>
